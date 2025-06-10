9c64507133f8c392c4519570bdeee0710b6a2b65
SionAlexander <sion.hartono04@gmail.com>
2025-06-11 06:45:39 +0700

    LoginScreen.jsx
    Login Admin

---
+++ LoginScreen.jsx
@@ -0,0 +1,11 @@
+//const handleLogin = () => {
+    //let userRole = 'employee';
+    //if (email === 'admin@shiftease.com' && password === 'admin') {
+      //userRole = 'admin';
+    //} else if (email === 'employee@shiftease.com' && password === 'employee') {
+      //userRole = 'employee';
+    //} else {
+      //WebAlert.alert('Login Failed', 'Invalid email or password.');
+      //return;
+    //}
+//}
